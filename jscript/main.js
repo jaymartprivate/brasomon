155b930e9984a759e23a4cd5714460192c61cdb8
jaymartprivate <60768136+jaymartprivate@users.noreply.github.com>
2020-03-08 19:00:11 +0800

    Add files via upload

---
+++ jscript/main.js
@@ -0,0 +1,64 @@
+var modal = document.getElementById("Modal1");
+var img = document.getElementById("feat1");
+var modalIng = document.getElementById("imgfeat1");
+var captionText = document.getElementById("caption1");
+
+img.onclick = function()
+{
+modal.style.display = "block";
+modalIng.src = this.src;
+captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0,1,2,3];
+span.onclick = function() 
+{
+modal.style.display = "none";
+} 
+var modal = document.getElementById("Modal2");
+var img = document.getElementById("feat2");
+var modalIng = document.getElementById("imgfeat2");
+var captionText = document.getElementById("caption2");
+
+img.onclick = function()
+{
+modal.style.display = "block";
+modalIng.src = this.src;
+captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0,1,2,3];
+span.onclick = function() 
+{
+modal.style.display = "none";
+} 
+var modal = document.getElementById("Modal3");
+var img = document.getElementById("feat3");
+var modalIng = document.getElementById("imgfeat3");
+var captionText = document.getElementById("caption3");
+
+img.onclick = function()
+{
+modal.style.display = "block";
+modalIng.src = this.src;
+captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0,1,2,3];
+span.onclick = function() 
+{
+modal.style.display = "none";
+} 
+var modal = document.getElementById("Modal4");
+var img = document.getElementById("feat4");
+var modalIng = document.getElementById("imgfeat4");
+var captionText = document.getElementById("caption4");
+
+img.onclick = function()
+{
+modal.style.display = "block";
+modalIng.src = this.src;
+captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0,1,2,3];
+span.onclick = function() 
+{
+modal.style.display = "none";
+} 
